2180bfdc830c00e6431ffa6e42cd4018f2ff4284
harrrrion <harrisonrigby@icloud.com>
2024-08-03 14:09:06 +0100

    fixed some errors

--- src/scanner.rs
+++ src/scanner.rs
@@ -85,8 +85,8 @@ impl fmt::Display for Literal {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Literal::Number(value) => write!(f, "{}", value),
-            Literal::String(value) => write!(f, "{}, string", value),
-            Literal::Bool(value) => write!(f, "{}", value),
+            Literal::String(value) => write!(f, "{}", value),
+            Literal::Bool(value) => write!(f, "{}", value.to_string()),
             Literal::Nil => write!(f, "nil"),
         }
     }
