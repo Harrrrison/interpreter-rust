a7247b7474cf20972ca731bba4910a6246b4b2f5
harrrrion <harrisonrigby@icloud.com>
2024-08-03 14:10:53 +0100

    fixed some errors

--- src/scanner.rs
+++ src/scanner.rs
@@ -82,11 +82,11 @@ impl fmt::Display for Token {
 }
 
 impl fmt::Display for Literal {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Literal::Number(value) => write!(f, "{}", value),
             Literal::String(value) => write!(f, "{}", value),
-            Literal::Bool(value) => write!(f, "{}", value.to_string()),
+            Literal::Bool(value) => write!(f, "{}", value),
             Literal::Nil => write!(f, "nil"),
         }
     }
