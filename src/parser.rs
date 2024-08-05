6eecc99412909a41a7302bdceaab0dd2fd022c1c
harrrrion <harrisonrigby@icloud.com>
2024-08-05 15:42:46 +0100

    fixed some errors

--- src/parser.rs
+++ src/parser.rs
@@ -119,7 +119,12 @@ impl<'a> std::fmt::Display for Expr {
             }
 
             Expr::Literal { value } => {
-                write!(f, "{}", value)
+                write!(f, "{}",
+                match value {
+                    Some(Literal::Number(literal))  => format!("{:?}", literal),
+                    Some(literal) => format!("{}", literal),
+                    None => "null".to_string(),
+                })
             }
 
             Expr::Unary { operator, right } => {
